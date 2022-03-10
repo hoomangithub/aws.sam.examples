13cbca83a35d28821cf024121fa065acdb8380b4
hoomangithub <hooman.paknahad@gmail.com>
2022-03-10 21:48:43 +0100

    add comments

--- orders-api-python/ordersapi/create.py
+++ orders-api-python/ordersapi/create.py
@@ -7,6 +7,7 @@ table_name = os.environ.get('ORDERS_TABLE')
 
 
 def lambda_handler(event, context):
+    # order is a dict (https://docs.python.org/3/library/json.html#py-to-json-table)
     order = json.loads(event['body'])
     table = dynamodb.Table(table_name)
     response = table.put_item(TableName=table_name, Item=order)
@@ -14,5 +15,6 @@ def lambda_handler(event, context):
     return{
         'statusCode': 201,
         'headers': {},
+        # json.dumps serializes a object
         'body': json.dumps({'message': 'Order Created'})
     }
--- orders-api-python/ordersapi/read.py
+++ orders-api-python/ordersapi/read.py
@@ -10,7 +10,9 @@ table_name = os.environ.get('ORDERS_TABLE')
 
 def lambda_handler(event, context):
     table = dynamodb.Table(table_name)
+    # defenierte request parameter in template.yaml (/orders/{id}) ist in 'pathParameters'
     order_id = int(event['pathParameters']['id'])
+    # table.query ist wie SELECT und KeyConditionExpression ist wie WHERE
     response = table.query(KeyConditionExpression=Key('id').eq(order_id))
 
     return{
